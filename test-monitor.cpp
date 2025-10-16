fb0e1c870bc91c886cbc721ec36fc4fa5a1476c8
hanishfire16-hue <hanishfire16@gmail.com>
2025-10-16 14:49:51 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,22 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+TEST(Monitor, TemperatureOutOfRange) {
+    EXPECT_FALSE(vitalsInRange(103, 80, 95));
+    EXPECT_FALSE(vitalsInRange(94, 80, 95));
+}
+
+TEST(Monitor, PulseRateOutOfRange) {
+    EXPECT_FALSE(vitalsInRange(98, 59, 95));
+    EXPECT_FALSE(vitalsInRange(98, 101, 95));
+}
+
+TEST(Monitor, Spo2OutOfRange) {
+    EXPECT_FALSE(vitalsInRange(98, 80, 89));
+}
+
+TEST(Monitor, AllVitalsOk) {
+    EXPECT_TRUE(vitalsInRange(98.1, 70, 98));
+    EXPECT_TRUE(vitalsInRange(95, 60, 90));
+    EXPECT_TRUE(vitalsInRange(102, 100, 90));
 }
